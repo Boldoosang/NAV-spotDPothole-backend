97c13a587336bbf0af639c063fa57a01d3b53c81
Boldoosang <jbaldeo@yahoo.com>
2022-01-30 04:59:53 -0400

    Included public facing firebase config

---
+++ App/firebaseConfig.py
@@ -0,0 +1,12 @@
+import os
+
+config = {
+  "apiKey": "AIzaSyD8jpQ-AMBIHpFwpUfNyITEndS0A-5WwOE",
+  "authDomain": "spotdpothole.firebaseapp.com",
+  "projectId": "spotdpothole",
+  "databaseURL": "",
+  "storageBucket": "spotdpothole.appspot.com",
+  "messagingSenderId": "500033402497",
+  "appId": "1:500033402497:web:e01a744396c50b8c033f80",
+  "serviceAccount" : "serviceAccountKey.json"
+}
